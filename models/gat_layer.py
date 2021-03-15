371ac2e6426486caf6b37c4c3f09ca4d39ec17c9
PavlosPiperis <58398272+PavlosPiperis@users.noreply.github.com>
2021-03-15 23:01:49 +0000

    gat_layer second draft

--- models/gat_layer.py
+++ models/gat_layer.py
@@ -12,29 +12,35 @@ class GATLayer(nn.Module):
     """
 
     # Extra possible params from torch_geometric: (add_self_loops: bool = True, bias: bool = True, **kwargs for MessagePassing)
-    def __init__(self,in_channels, out_channels, dropout, alpha, concat = True):
+    def __init__(self, in_channels, out_channels, dropout, alpha, number_of_heads, concat=True, include_skip_connection = False):
         super(GATLayer, self).__init__()
 
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.dropout = dropout # should be 0.6
-        self.alpha = alpha # should be 0.2
+        self.dropout = dropout  # should be 0.6
+        self.alpha = alpha  # should be 0.2
+        self.number_of_heads = number_of_heads
         self.concat = concat
+        self.include_skip_connection = include_skip_connection # useful for the transductive implementation
 
         # model should be initialised using Glorot(Xavier initialisation) which means:
         # leakyReLu with negative slope = 0.2
         # there is also an in-built initialisation in pytorch for Xavier initialisation
 
         self.weights = nn.Parameter(torch.zeros(size=(in_channels, out_channels)))
-        gain = nn.init.calculate_gain('relu') # this should return sqrt(2), which is the recommended value for the
-                                              # non-linearity function relu
+        gain = nn.init.calculate_gain('relu')  # this should return sqrt(2), which is the recommended value for the
+                                               # non-linearity function relu
 
         nn.init.xavier_uniform_(self.weights.data, gain=gain)
-        self.attention = nn.Parameter(torch.zeros(size=(2*out_channels,1))) # 2 because we will concatenate the two
-                                                                            # representations and we need to match the
-                                                                            # dimension
+        self.attention = nn.Parameter(torch.zeros(size=(2 * out_channels, 1))) # 2 because we will concatenate the two
+                                                                               # representations and we need to match the
+                                                                               # dimension
 
         nn.init.xavier_uniform_(self.attention.data, gain=gain)
+        1
+        if include_skip_connection_skip_connection:
+            self.skip_projections = nn.Linear(in_channels, no_of_heads * out_channels, bias=False)
+
         self.leakyReLu = nn.LeakyReLU(self.alpha)
         self.reset_parameters()
 
@@ -43,25 +49,20 @@ class GATLayer(nn.Module):
         # not very sure what to put here maybe the xavier weights?
 
     def forward(self, x, immediate_neighbor):
-        h = torch.mm(x, self.weights) # the initial linear transformation
-        n = h.size()[0] # extracting the size of h (F in the paper)
+        h = torch.mm(x, self.weights)  # the initial linear transformation
+        n = h.size()[0]  # extracting the size of h (F in the paper)
 
         # attention
-        input_attention = torch.cat([h.repeat(1, n).view(n*n, -1), h.repeat(n, 1)], dim=1).view(n, -1, 2*self.out_channels)
-        
+        input_attention = torch.cat([h.repeat(1, n).view(n * n, -1), h.repeat(n, 1)], dim=1).view(n, -1, 2 * self.out_channels)
+
         attention_coefficients = self.leakyReLu(torch.matmul(input_attention, self.attention))
         attention_coefficients = attention_coefficients.squeeze(2)
-        
-        # masked attention
-        #negative_vector = -10e20*torch.ones_like(attention_coefficients)
-        masked_attention = torch.where(immediate_neighbor > 0, attention_coefficients, 0.)
 
-                                         # we have attention only for immediate neighbors, otherwise we replace them
-                                         # with 0. NOTE: instead of 0 maybe we can put something really negative,
-                                         # like negative_vector
-        # for immediate neighbors
-        # otherwise we replace with 0
-        # here instead of 0 maybe
+        # masked attention
+        negative_vector = -10e20*torch.ones_like(attention_coefficients)
+        masked_attention = torch.where(immediate_neighbor > 0, attention_coefficients, negative_vector) # to avoid any
+                                                                # computation with 0, as that might create problems with
+                                                                # backpropagation and attention updates
 
         masked_attention = F.softmax(masked_attention, dim=1)
         masked_attention = F.dropout(masked_attention, self.dropout, training=self.training)
@@ -74,4 +75,17 @@ class GATLayer(nn.Module):
             return F.elu(h_hat)
         else:
             return h_hat
+
+    def skip_connections(self, input_node_features, output_node_features):
+
+        if self.include_skip_connection:
+            if output_node_features.shape[-1] == input_node_features.shape[-1]:
+                output_node_features += input_node_features.unsqueeze(1)
+            else:
+                output_node_features += self.skip_projection(input_nodes_features).view(-1, self.number_of_heads, self.out_channels)
+
+        if self.concat:
+            output_node_features = output_node_features.view(-1, self.number_of_heads * self.out_channels)
+        else:
+            output_node_features = output_node_features.mean(dim=1)
 
