c25d09b83e0c0749562d32ddeb997f0e9678bf52
Oliver Warwick <oliverwarwick6@gmail.com>
2021-03-18 20:30:40 +0000

    broken version using Alaka code

--- models/indu_gat.py
+++ models/indu_gat.py
@@ -3,10 +3,9 @@ import sys
 import torch.nn.functional as F
 import argparse
 from torch_geometric.datasets import PPI
-from torch_geometric.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 from torch_geometric.nn import GATConv
-from torch.nn import Sigmoid
-from torch.nn import BCEWithLogitsLoss
+from torch.nn import Sigmoid, Linear, BCEWithLogitsLoss
 import pytorch_lightning as pl
 from gat_layer import GATLayer
 from sklearn.metrics import f1_score
@@ -18,7 +17,76 @@ from sklearn.metrics import f1_score
 
 # pl.seed_everything(42)
 
-# OW test
+# Taken from AI Emp.
+# OW - will remove just trying to make sure it works
+class GraphDataLoader(DataLoader):
+    """
+    When dealing with batches it's always a good idea to inherit from PyTorch's provided classes (Dataset/DataLoader).
+
+    """
+    def __init__(self, node_features_list, node_labels_list, edge_index_list, batch_size=1, shuffle=False):
+        graph_dataset = GraphDataset(node_features_list, node_labels_list, edge_index_list)
+        # We need to specify a custom collate function, it doesn't work with the default one
+        super().__init__(graph_dataset, batch_size, shuffle, collate_fn=graph_collate_fn)
+
+
+class GraphDataset(Dataset):
+    """
+    This one just fetches a single graph from the split when GraphDataLoader "asks" it
+
+    """
+    def __init__(self, node_features_list, node_labels_list, edge_index_list):
+        self.node_features_list = node_features_list
+        self.node_labels_list = node_labels_list
+        self.edge_index_list = edge_index_list
+        print("creating dataset")
+
+    # 2 interface functions that need to be defined are len and getitem so that DataLoader can do it's magic
+    def __len__(self):
+        return len(self.edge_index_list)
+
+    def __getitem__(self, idx):  # we just fetch a single graph
+        print(idx)
+        return self.node_features_list[idx], self.node_labels_list[idx], self.edge_index_list[idx]
+
+
+def graph_collate_fn(batch):
+    """
+    The main idea here is to take multiple graphs from PPI as defined by the batch size
+    and merge them into a single graph with multiple connected components.
+
+    It's important to adjust the node ids in edge indices such that they form a consecutive range. Otherwise
+    the scatter functions in the implementation 3 will fail.
+
+    :param batch: contains a list of edge_index, node_features, node_labels tuples (as provided by the GraphDataset)
+    """
+    print(batch)
+    edge_index_list = []
+    node_features_list = []
+    node_labels_list = []
+    num_nodes_seen = 0
+
+    for features_labels_edge_index_tuple in batch:
+        # Just collect these into separate lists
+        node_features_list.append(features_labels_edge_index_tuple[0])
+        node_labels_list.append(features_labels_edge_index_tuple[1])
+
+        edge_index = features_labels_edge_index_tuple[2]  # all of the components are in the [0, N] range
+        edge_index_list.append(edge_index + num_nodes_seen)  # very important! translate the range of this component
+        num_nodes_seen += len(features_labels_edge_index_tuple[1])  # update the number of nodes we've seen so far
+
+    # Merge the PPI graphs into a single graph with multiple connected components
+    node_features = torch.cat(node_features_list, 0)
+    node_labels = torch.cat(node_labels_list, 0)
+    edge_index = torch.cat(edge_index_list, 1)
+
+    print("graph coalate function")
+    print(node_features.shape)
+    print(node_labels.shape)
+    print(edge_index.shape)
+
+    return node_features, node_labels, edge_index
+
 
 class induGAT(pl.LightningModule):
     def __init__(self, dataset, node_features, num_classes, in_heads=4, mid_heads=4, out_heads=6, head_features=256, l2_reg=0, lr = 0.005, dropout=0):
@@ -40,27 +108,20 @@ class induGAT(pl.LightningModule):
         self.gat1 = GATConv(in_channels=self.node_features, out_channels=self.head_features, add_self_loops=True, heads=self.in_heads)#, add_self_loops=True, dropout=self.dropout) 
         self.gat2 = GATConv(in_channels=self.head_features * self.in_heads, out_channels=self.head_features, heads=self.mid_heads) #dropout=self.dropout) 
         self.gat3 = GATConv(in_channels=self.head_features * self.mid_heads, out_channels=self.num_classes, heads=out_heads, concat=False)#, dropout=self.dropout)
+        self.skip_connection = Linear(self.head_features * self.in_heads, self.head_features * self.mid_heads, bias=False)
 
         # These will be initialised in prepare_data
         self.train_ds, self.val_ds, self.test_ds = None, None, None
 
 
     def forward(self, data):
-        x, edge_index = data.x, data.edge_index
-
+        x, edge_index = data[0], data[2]
         x = self.gat1(x, edge_index)
         x = F.elu(x)
-        # layer1_skip = x  # add skip connection between layer 1 output and layer 3 input
-        # print(layer1_skip)
+        skip_values = self.skip_connection(x)
         x = self.gat2(x, edge_index)
         x = F.elu(x)
-        # print(layer1_skip)
-        x = self.gat3(x , edge_index) #+ layer1_skip
-        # print(x)
-        # s = Sigmoid()
-        # x = s(x)
-        # print(x)
-
+        x = self.gat3(x + skip_values, edge_index) #+ layer1_skip
         return x
 
     
@@ -69,13 +130,15 @@ class induGAT(pl.LightningModule):
         return optimizer
 
     def training_step(self, batch, batch_idx):
+        print(batch)
+        print(batch_idx)
         out = self(batch)
         # print("sigmoid error: ", (out[0]-batch.y[0]))
         loss_fn = BCEWithLogitsLoss(reduction='mean') #F.binary_cross_entropy(out, batch.y)
-        loss = loss_fn(out, batch.y)
+        loss = loss_fn(out, batch[1])
         self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         
-        f1 = f1_score(y_pred=out > 0, y_true=batch.y, average='micro')
+        f1 = f1_score(y_pred=out > 0, y_true=batch[1], average='micro')
         self.log('train_f1_score', f1, on_step=True, on_epoch=True, prog_bar=True, logger=True)
 
         return loss
@@ -84,10 +147,10 @@ class induGAT(pl.LightningModule):
         out = self(batch)
         # loss = F.binary_cross_entropy(out, batch.y)
         loss_fn = BCEWithLogitsLoss(reduction='mean') #F.binary_cross_entropy(out, batch.y)
-        loss = loss_fn(out, batch.y)
+        loss = loss_fn(out, batch[1])
         self.log('val_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         
-        f1 = f1_score(y_pred=out > 0, y_true=batch.y, average="micro")
+        f1 = f1_score(y_pred=out > 0, y_true=batch[1], average="micro")
         self.log('val_f1_score', f1, on_step=True, on_epoch=True, prog_bar=True, logger=True)
 
         return loss
@@ -97,7 +160,7 @@ class induGAT(pl.LightningModule):
         out = self(batch)
         pred = (out > 0)
 
-        f1 = f1_score(y_pred=pred, y_true=batch.y, average="micro")
+        f1 = f1_score(y_pred=pred, y_true=batch[1], average="micro")
         self.log('val_f1_score', f1, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         print(f1)
         # TODO can add accuracy/precision/recall although not sure how that aggregates in multilabel setting
@@ -107,22 +170,69 @@ class induGAT(pl.LightningModule):
     def prepare_data(self):
         self.train_ds = PPI(root='/tmp/PPI', split='train')
         self.val_ds = PPI(root='/tmp/PPI', split='val')
-        # N = len(train_ds.dataset)
-        # train_size = 0.75*N
-        # # Split into train and val
-        # self.train_ds, self.val_ds = torch.utils.data.random_split(train_ds, [train_size, N-train_size])
-
         self.test_ds = PPI(root='/tmp/PPI', split='test')
 
     # Only for PPI dataset at this stage - move into train.py
     def train_dataloader(self):
-        return DataLoader(self.train_ds, batch_size=2, shuffle=True)        
+        # print("Calling val train loader.")
+        node_features_list, node_label_list, edge_index_list = reconfig_data(self.train_ds)
+        print('finished reconfiguring training data')
+        # print("Edge data being passed:")        
+        print("data loader train object: ")
+        print(GraphDataLoader(node_features_list, node_label_list, edge_index_list, batch_size=2, shuffle=True).__dict__)
+        return GraphDataLoader(node_features_list, node_label_list, edge_index_list, batch_size=2, shuffle=True)        
     
     def val_dataloader(self):
-        return DataLoader(self.val_ds) 
+        # print("Calling val data loader.")
+        node_features_list, node_label_list, edge_index_list = reconfig_data(self.val_ds)
+        print('finished reconfiguring val data')
+
+        # print("Edge data being passed:")
+        # for e in edge_index_list:
+        #     print(e.shape)
+        return GraphDataLoader(node_features_list, node_label_list, edge_index_list, batch_size=1) 
 
     def test_dataloader(self):
-        return DataLoader(self.test_ds)
+        # print("Calling val test loader.")
+        node_features_list, node_label_list, edge_index_list = reconfig_data(self.test_ds)
+        print('finished reconfiguring test data')
+
+        # print("Edge data being passed:")      
+        # for e in edge_index_list:
+        #     print(e.shape)
+        return GraphDataLoader(node_features_list, node_label_list, edge_index_list, batch_size=1)
+
+
+
+
+def reconfig_data(loaded_dataset):
+    # Takes in something like self.val_ds and returns the lists we need.
+    node_features_list = [] 
+    for tensor_index, i in enumerate(loaded_dataset.slices['x'].numpy()):
+        if i+1 < len(loaded_dataset.slices['x'].numpy()):
+            node_features_list.append(loaded_dataset.data.x[tensor_index:loaded_dataset.slices['x'].numpy()[i+1]])
+        else:
+            node_features_list.append(loaded_dataset.data.x[tensor_index:])
+    node_label_list = []
+    for tensor_index, i in enumerate(loaded_dataset.slices['y'].numpy()):
+        if i+1 < len(loaded_dataset.slices['y'].numpy()):
+            node_label_list.append(loaded_dataset.data.y[tensor_index:loaded_dataset.slices['y'].numpy()[i+1]])
+        else:
+            node_label_list.append(loaded_dataset.data.y[tensor_index:])
+    edge_index_list = []
+    for tensor_index, i in enumerate(loaded_dataset.slices['edge_index'].numpy()):
+        if i+1 < len(loaded_dataset.slices['edge_index'].numpy()):
+            edge_index_list.append(loaded_dataset.data.edge_index[0:2, tensor_index:loaded_dataset.slices['edge_index'].numpy()[i+1]])
+        else:
+            edge_index_list.append(loaded_dataset.data.edge_index[0:2, tensor_index:])
+    # print("Node label: ")
+    # print(node_label_list)
+    # print("Features list: ")
+    # print(node_features_list)
+    # print("Edge List: ")
+    # print(edge_index_list)
+    return (node_features_list, node_label_list, edge_index_list)
+
 
 if __name__ == "__main__":
     gat = induGAT(dataset='PPI', node_features=50, num_classes=121, lr=0.005, l2_reg=0)
