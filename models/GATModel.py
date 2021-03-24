if skip_in==skip_out -> Identity. fixing skip connection logging to not log gradients of identity

models/GATModel.py
@@ -117,7 +117,7 @@ class GATModel(pl.LightningModule):
                     # Add a linear projection from NH(l-1) * F_OUT(l-1) to F_OUT(l).
                     skip_out = self.head_output_features_per_layer[i+1]
 
-                if skip_in != skip_out:
+                if skip_in == skip_out:
                     skip_layer = nn.Identity()
                 else:
                     skip_layer = nn.Linear(in_features=skip_in, out_features=skip_out, bias=False)

models/indu_gat.py
@@ -1,4 +1,5 @@
 import torch
+from torch import nn
 from torch.nn import BCEWithLogitsLoss
 from torch_geometric.data import DataLoader
 from torch_geometric.datasets import PPI
@@ -62,7 +63,9 @@ class induGAT(GATModel):
                     tensorboard.experiment.add_histogram(f"gat_weight_layer{i}", self.gat_layer_list[i].W.weight.grad)
                     tensorboard.experiment.add_histogram(f"attention_weight_layer{i}", self.gat_layer_list[i].a.weight.grad)
                     if len(self.skip_layer_list) > skip_count:
-                        tensorboard.experiment.add_histogram(f"skip_weight_layer{i}", self.skip_layer_list[skip_count].weight.grad)
+                        skip_layer = self.skip_layer_list[skip_count]
+                        if isinstance(skip_layer, torch.nn.Linear):
+                            tensorboard.experiment.add_histogram(f"skip_weight_layer{i}", skip_layer.weight.grad)
                         skip_count += 1
 
     #     print("On backwards")